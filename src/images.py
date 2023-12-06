13a03b94b14301b1929f90c987ddb2489c6f98ba
Kelsyn Carter <eannacarter35@gmail.com>
2023-12-06 10:16:45 -0500

    Renamed the blue assets so it makes more sense

--- src/images.py
+++ src/images.py
@@ -4,7 +4,7 @@ from constants import Colors, Sizes, Fonts
 
 
 # Creates snake p1 body
-snake_body = pygame.image.load('assets\snake_body.png').convert_alpha()
+snake_body = pygame.image.load('assets\snake_body_blue.png').convert_alpha()
 snake_body = pygame.transform.smoothscale(snake_body, (Sizes.SNAKE_BLOCK, Sizes.SNAKE_BLOCK))
 
 # Creates the apple image
