16f92d1579e41e7e49ded6e098d0da71589e249b
henrikvtcodes <commits@henrikvt.com>
2023-12-06 13:31:53 -0500

    add screen size warning

--- src/constants.py
+++ src/constants.py
@@ -24,12 +24,10 @@ class Fonts():
 # Global title text object
 TITLE_TEXT = Fonts.SCORE_FONT.render("Intellisnake", True, Colors.WHITE)
 
-
 class Sizes():
-    """ This class contains all the dimensions for the game."""
+    """ This class contains all the dimensions for the game. DO NOT CHANGE, GAME WILL BREAK."""
     SCREEN_WIDTH = 1000
     SCREEN_HEIGHT = 600
-    """ Screen Sizes (width,height): (640, 480), (800, 600), (1024, 768), (1280, 720)"""
     
     SCORE_DISPLAY_HEIGHT = SCREEN_HEIGHT // 10
     GAME_DISPLAY_HEIGHT = SCREEN_HEIGHT - SCORE_DISPLAY_HEIGHT
