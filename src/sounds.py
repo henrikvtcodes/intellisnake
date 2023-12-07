8b95f12eb72e021744717781600b78c9d7831dcc
henrikvtcodes <commits@henrikvt.com>
2023-12-06 23:20:44 -0500

    set up sounds

---
+++ src/sounds.py
@@ -0,0 +1,14 @@
+import platform, pygame
+
+pygame.init()
+
+if platform.system() == "Windows":
+  pygame.mixer.init()
+  willhelm = pygame.mixer.Sound("assets\willhelm.wav")
+  mlem = pygame.mixer.Sound("assets\mlem.wav")
+  win = pygame.mixer.Sound("assets\win.wav")
+else:
+  pygame.mixer.init()
+  willhelm = pygame.mixer.Sound("assets/willhelm.wav")
+  mlem = pygame.mixer.Sound("assets/mlem.wav")
+  win = pygame.mixer.Sound("assets/win.wav")
