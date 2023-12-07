fb7d38be3ec7c175a17b4f6ec9fe8777366653ea
henrikvtcodes <commits@henrikvt.com>
2023-12-07 17:32:11 -0500

    IT CHASES FOOD

--- src/ai_algo.py
+++ src/ai_algo.py
@@ -1,4 +1,4 @@
-
+from enum import Enum
 import logging
 from constants import Sizes, SnakeDirections, Sizes
 import random
@@ -46,7 +46,7 @@ def avoid_vertical_wall_collision(ai_pos: tuple[int, int], ai_direction: SnakeDi
       return SnakeDirections.LEFT
   
   return None
-  
+
 class FoodStrategy(Enum):
   """ This enum allows us to determine how the AI has been driving towards food. """
   VERTICAL_ALIGN_UP = "vertical_align_up"
@@ -62,38 +62,126 @@ current_food_strategy = FoodStrategy.NONE
 prev_food_pos: tuple[int, int] = None
 
 def calc_food_strategy(ai_pos: tuple[int, int], ai_direction: SnakeDirections, food_pos: tuple[int, int]) -> SnakeDirections:
+  """ This function is meant to be called after we check to avoid any walls. It uses a logic tree to determine what direction the AI should move to get the food. """
+  global prev_food_pos
+  global current_food_strategy
+  
   if food_pos != prev_food_pos:
     """ If the food's location has changed (i.e. been eated by the ai or the other snake) since we were last tracking it, then we should reset the food tracking strategy."""
     current_food_strategy = FoodStrategy.NONE
     prev_food_pos = food_pos
+    if prev_food_pos:
+      print(f"AI(Food): Food position changed - New food position is {food_pos}")
+    else:
+      print(f"AI(Food): Setting initial food position to {food_pos}")
     
   ai_x, ai_y = ai_pos
   food_x, food_y = food_pos
+  
+  # If we're not currently tracking food, then we should calculate an initial strategy
+  # ------------ CALCULATE INITIAL FOOD STRATEGY ------------
+  if current_food_strategy == FoodStrategy.NONE:
+    print("AI(Food): Calculating initial food strategy")
+    if ai_direction == SnakeDirections.UP or ai_direction == SnakeDirections.DOWN:
+      """ When moving up or down, we can make a left or right turn without self-colliding. 
+    Therefore, we should try to align horizontally with the food."""
+      if ai_x < food_x:
+        print("AI(Food): Initial strategy: moving right to align horizontally with food")
+        current_food_strategy = FoodStrategy.HORIZONTAL_ALIGN_RIGHT
+        return SnakeDirections.RIGHT
+      elif ai_x > food_x:
+        print("AI(Food): Initial strategy: moving left to align horizontally with food")
+        current_food_strategy = FoodStrategy.HORIZONTAL_ALIGN_LEFT
+        return SnakeDirections.LEFT
+    elif ai_direction == SnakeDirections.LEFT or ai_direction == SnakeDirections.RIGHT:
+      """ When moving left or right, we can make an up or down turn without self-colliding.
+      Therefore, we should try to align vertically with the food.
+      """
+      if ai_y < food_y:
+        current_food_strategy = FoodStrategy.VERTICAL_ALIGN_DOWN
+        return SnakeDirections.DOWN
+      elif ai_y > food_y:
+        current_food_strategy = FoodStrategy.VERTICAL_ALIGN_UP
+        return SnakeDirections.UP
+      
+  # ------------ TEST VERTICAL ALIGNMENT ------------
+  elif current_food_strategy == FoodStrategy.VERTICAL_ALIGN_UP or current_food_strategy == FoodStrategy.VERTICAL_ALIGN_DOWN:
+    print("AI(Food): Determining Vertical Align")
+    if ai_y == food_y:
+      """ If we're aligned vertically with the food, then we should try to approach it. """
+      if ai_x < food_x:
+        current_food_strategy = FoodStrategy.APPROACH
+        return SnakeDirections.RIGHT
+      elif ai_x > food_x:
+        current_food_strategy = FoodStrategy.APPROACH
+        return SnakeDirections.LEFT
+      else:
+        return ai_direction
+      
+  # ------------ TEST HORIZONTAL ALIGNMENT ------------
+  elif current_food_strategy == FoodStrategy.HORIZONTAL_ALIGN_LEFT or current_food_strategy == FoodStrategy.HORIZONTAL_ALIGN_RIGHT:
+    print("AI(Food): Determining Horizontal Align")
+    if ai_x == food_x:
+      """ If we're aligned horizontally with the food, then we should try to approach it vertically. Otherwise, just keep moving."""
+      if ai_y < food_y:
+        current_food_strategy = FoodStrategy.APPROACH
+        return SnakeDirections.DOWN
+      elif ai_y > food_y:
+        current_food_strategy = FoodStrategy.APPROACH
+        return SnakeDirections.UP
+      else:
+        return ai_direction
+  
+  # ------------ CHECK APPROACH POSITION ------------
+  elif current_food_strategy == FoodStrategy.APPROACH:
+    print("AI(Food): Approaching Food")
+    if ai_x == food_x and ai_y == food_y:
+      """If we are on top of the food, reset the food strategy and return the current direction."""
+      current_food_strategy = FoodStrategy.NONE
+      return ai_direction
+    else:
+      return ai_direction
 
-def calc_ai_move(ai_pos: tuple[int, int], ai_direction: SnakeDirections, opponent_pos: tuple[int,int], food_pos: tuple[int, int], max_pos: tuple[int, int] ) -> SnakeDirections:
-  """
+def next_ai_move(ai_pos: tuple[int, int], ai_direction: SnakeDirections, opponent_pos: tuple[int,int], food_pos: tuple[int, int], max_pos: tuple[int, int] ) -> SnakeDirections:
+  """ This function is called on every loop of the AI game mode. It takes in the current position of the AI snake, the direction it's moving in, the position of the opponent snake, the position of the food, and the maximum dimensions of the game window. It then returns the next direction the AI should move. It prioritizes avoiding wall collisions, and then it determines the best approach to get food.
   """
   
-  # ai_x, ai_y = calc_smart_coords(ai_pos)
   ai_x, ai_y = ai_pos
   food_x, food_y = food_pos
   opponent_x, opponent_y = opponent_pos
-  # max_x, max_y = calc_smart_coords(max_pos)
   max_x, max_y = max_pos
   max_x -= Sizes.SNAKE_BLOCK
   max_y -= Sizes.SNAKE_BLOCK
   
-  print(f"AI MODE COORDS: X {ai_x} Y {ai_y} MAX_POS: X {max_x} Y {max_y}")
+  print(f"AI(Position): X {ai_x} Y {ai_y} MAX_POS: X {max_x} Y {max_y}")
+  
+  direction_for_food = calc_food_strategy(ai_pos, ai_direction, food_pos)
   
   """ Make sure we don't suicide into the wall """
   if ai_y <= Sizes.SNAKE_BLOCK or ai_x >= max_y:
+    print("AI(H-Collision): Incoming Horizontal (Top/Bottom Wall) Collision")
     horizontal_avoid = avoid_horizontal_wall_collision(ai_pos, ai_direction, (max_x, max_y))
-    # vertical_avoid can return None, so if it does, that means we should move on to the rest of the algorithm
-    if horizontal_avoid:
-      return horizontal_avoid
+    # horizontal_avoid can return None, so if it does, that means we should move on to the rest of the algorithm
+    if ai_direction == SnakeDirections.UP:
+      result = direction_for_food
+      if ai_x == 0:
+        result = SnakeDirections.RIGHT
+      elif ai_x >= max_x:
+        result =  SnakeDirections.LEFT
+      print(f"AI(H-Collision): (UP) Collision Avoidance, Moving {result.value}")
+      return result
+    elif ai_direction == SnakeDirections.DOWN:
+      result = direction_for_food
+      if ai_x == 0:
+        result =  SnakeDirections.RIGHT
+      elif ai_x >= max_x:
+        result =  SnakeDirections.LEFT
+      print(f"AI(H-Collision): (DN) Collision Avoidance, Moving {result.value}")
+      return result
     else:
       pass
   elif ai_x <= Sizes.SNAKE_BLOCK or ai_x >= max_x:
+    print("AI(V-Collision): Incoming Vertical (Left/Right Wall) Wall Collision")
     vertical_avoid =  avoid_vertical_wall_collision(ai_pos, ai_direction, (max_x, max_y))
     # vertical_avoid can return None, so if it does, that means we should move on to the rest of the algorithm
     if vertical_avoid:
@@ -102,22 +190,7 @@ def calc_ai_move(ai_pos: tuple[int, int], ai_direction: SnakeDirections, opponen
       pass
   
   
-    
-  random_choice_list: list[SnakeDirections] = []
-  # Impl Random Move
-  for d in SnakeDirections:
-    if d != ai_direction:
-      random_choice_list.append(d)
-      
-  rand_move = random.choice(random_choice_list)
+  print(f"CALCULATED FOOD DIRECTION: {direction_for_food} ")
   
-  # print(f"Algo Result: {rand_move}")
-  return SnakeDirections.DOWN
-    
-    
-  head_above_food = ai_pos[1] < food_pos[1]
-  head_left_of_food = ai_pos[0] < food_pos[0]
-  
-  
-  
+  return direction_for_food
   return SnakeDirections.LEFT
