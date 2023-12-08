345a0f99df934f7bf1f58cdf4e019114ce637460
Kelsyn Carter <eannacarter35@gmail.com>
2023-12-07 22:51:52 -0500

    Commented the new ai algo file What do we do with the other one?

--- src/ai_algo_legend.py
+++ src/ai_algo_legend.py
@@ -1,5 +1,8 @@
 from constants import *
 
+# checks if the given direction will cause the 
+# ai snake to hit itself or the other snake on the
+# next move
 def check_collison_course(opp_snake, my_snake, x, y, cur_dir):
     print(cur_dir)
     if cur_dir == SnakeDirections.DOWN:
@@ -10,10 +13,9 @@ def check_collison_course(opp_snake, my_snake, x, y, cur_dir):
         x -= Sizes.SNAKE_BLOCK
     if cur_dir == SnakeDirections.RIGHT:
         x += Sizes.SNAKE_BLOCK
- 
+    
     for i in opp_snake:
         if i[0] == x and i[1] == y:
-            print("Collision immenent!")
             return True
     for k in my_snake:
         if k[0] == x and k[1] == y:
@@ -21,14 +23,16 @@ def check_collison_course(opp_snake, my_snake, x, y, cur_dir):
     return False
 
 
-
+# generates the ai's next move
 def generate_next_move(opp_snake, my_snake, x, y, goal_x, goal_y, cur_dir):
 
+    # sets the possible return values
     up = (0, -Sizes.SNAKE_BLOCK)
     down = (0, Sizes.SNAKE_BLOCK)
     left = (-Sizes.SNAKE_BLOCK, 0)
     right = (Sizes.SNAKE_BLOCK, 0)
 
+    # sets the desired directions for finding the grapes
     if x > goal_x:
         desired_x = left
     elif x < goal_x:
@@ -42,114 +46,141 @@ def generate_next_move(opp_snake, my_snake, x, y, goal_x, goal_y, cur_dir):
     else:
         desired_y = down
 
+    # oob = out of bounds
+    # defaults all oob values to false
     oob_up = False
     oob_down = False
     oob_left = False
     oob_right = False
-    # Checks out of bounds
+
+    # Checks if the snake is on the edge,
+    # if it does sets the oob for that side to true
     if x == Sizes.SCREEN_WIDTH - Sizes.SNAKE_BLOCK:
         oob_right = True
-        print("oob right")
+
     if x < 20:
         oob_left = True
-        print("oob left")
+
     if y == Sizes.SCREEN_HEIGHT - Sizes.SNAKE_BLOCK:
         oob_down = True
-        print("oob down")
+
     if y < 20: 
-        print("oob up")
+
         oob_up = True
     
     if oob_right:
         if cur_dir == SnakeDirections.RIGHT:
-            if oob_down:
-                print("oob right and down")
+            # if going right and in the right lower corner
+            # moves it up (only option)
+            if oob_down: 
                 return up
+            # if going right and in the right upper corner
             elif oob_up:
-                print("oob right and up")
                 return down
+            # checks if going up will send snake into another snake
             elif check_collison_course(opp_snake, my_snake, x, y, SnakeDirections.UP):
                 return down
+            # checks if going down will send snake into another snake
             elif check_collison_course(opp_snake, my_snake, x, y, SnakeDirections.DOWN):
                 return up
+            # otherwise tries to go towards grape
             else:
                 return desired_y
         
+    
     if oob_left:
         print(cur_dir)
         if cur_dir == SnakeDirections.LEFT:
+            # if going left and in the left lower corner
+            # moves it up (only maybe safe option)
             if oob_down:
-                print("Oob left and down")
                 return up
+            # if going left and in the left upper corner
             elif oob_up:
-                print("oob left and up")
                 return down
+            # checks if going up will send snake into another snake
             elif check_collison_course(opp_snake, my_snake, x, y, SnakeDirections.UP):
                 return down
+            # checks if going down will send snake into another snake
             elif check_collison_course(opp_snake, my_snake, x, y, SnakeDirections.DOWN):
                 return up
+            # otherwise tries to go towards grape
             else:
                 return desired_y
         
     if oob_up:
         if cur_dir == SnakeDirections.UP:
+            # if going up and in the right upper corner
             if oob_right:
-                print("oob up and right")
                 return left
+            # if going up and in the left upper corner
             elif oob_left:
-                print("oob up and left")
                 return right
+            # checks if going left will send snake into another snake
             elif check_collison_course(opp_snake, my_snake, x, y, SnakeDirections.LEFT):
                 return right
+            # checks if going right will send snake into another snake
             elif check_collison_course(opp_snake, my_snake, x, y, SnakeDirections.RIGHT):
                 return left
+            # otherwise tries to go towards grape
             else:
                 return desired_x
             
     if oob_down:
         if cur_dir == SnakeDirections.DOWN:
+            # if going down and in the right lower corner
             if oob_right:
-                print("oob down and right")
                 return left
+            # if going down and in the left lower corner
             elif oob_left:
-                print("oob down and left")
                 return right
+            # checks if going left will send snake into another snake
             elif check_collison_course(opp_snake, my_snake, x, y, SnakeDirections.LEFT):
                 return right
+            # checks if going right will send snake into another snake
             elif check_collison_course(opp_snake, my_snake, x, y, SnakeDirections.RIGHT):
                 return left
+            # otherwise tries to go towards grape
             else:
                 return desired_x
-    
+           
+    # checks if the snakes current course will send it out of bounds
     if check_collison_course(opp_snake, my_snake, x, y, cur_dir):
-        print("Collision warning received")
+        # if the snake is going up or down, decides whether to go left or right
         if cur_dir == SnakeDirections.UP or cur_dir == SnakeDirections.DOWN:
-            print("issue is here 1")
+            # makes sure direction change doesn't send snake out of bounds
             if oob_left:
                 return right
             elif oob_right:
                 return left
+            # checks if going left will send snake into another snake
             elif check_collison_course(opp_snake, my_snake, x, y, SnakeDirections.LEFT):
                 return right
+            # checks if going right will send snake into another snake
             elif check_collison_course(opp_snake, my_snake, x, y, SnakeDirections.RIGHT):
                 return left
+            # otherwise tries to go towards grape
             else:
                 return desired_x
             
-            
+        # if the snake is going left or right, decides whether to go up or down
         elif cur_dir == SnakeDirections.LEFT or cur_dir == SnakeDirections.RIGHT:
-            print("issue is here 2")
+            # makes sure direction change doesn't send snake out of bounds
             if oob_down:
                 return up
             elif oob_up:
                 return down
+            # checks if going up will send snake into a snake
             elif check_collison_course(opp_snake, my_snake, x, y, SnakeDirections.UP):
                 return down
+            # checks if going down will send snake into a snake
             elif check_collison_course(opp_snake, my_snake, x, y, SnakeDirections.DOWN):
                 return up
+            # otherwise tries to go towards grape
             else:
                 return desired_y
 
+    # sees if changing direction will get the snake closer to grape
     if cur_dir == SnakeDirections.UP or cur_dir == SnakeDirections.DOWN:
         if x > goal_x:
             if not check_collison_course(opp_snake, my_snake, x, y, SnakeDirections.LEFT) and not oob_left:
@@ -165,7 +196,5 @@ def generate_next_move(opp_snake, my_snake, x, y, goal_x, goal_y, cur_dir):
             if not check_collison_course(opp_snake, my_snake, x, y, SnakeDirections.DOWN) and not oob_down:
                 return down
     
-    print("no oob detected")
+    # if not in danger and no direction change, returns 0, 0
     return (0, 0)
-
-    # Tries to move to grape
